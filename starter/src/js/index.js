6b2d7b9b7feced9a901624ee7ec7e5f1d127e052
Reiniel Ravelo <reinielravelo@gmail.com>
2019-05-21 18:41:49 -0400

    Search.js file setup

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,5 +1,15 @@
 // Search API URL - https://www.food2fork.com/api/search
 // API key bf329327176803459447269e4bb95237
 
+import Search from './models/Search';
+
+const pizza = new Search('pizza'); 
+const nachos = new Search('nachos'); 
+
+console.log(pizza); 
+pizza.getResults(); 
+console.log(nachos); 
+nachos.getResults(); 
+
 
 
--- starter/src/js/models/Search.js
+++ starter/src/js/models/Search.js
@@ -1,2 +1,21 @@
+import axios from 'axios'; 
 
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
 
+    async getResults() {
+        const key = "bf329327176803459447269e4bb95237"; 
+        try {
+        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`); 
+       this.recipes = res.data.recipes; 
+        console.log(this.recipes); 
+        } catch (error) {
+        alert(error); 
+        }
+    }
+
+}
+
+             
