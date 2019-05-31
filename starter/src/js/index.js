d035d5f87698e6015bdadfdbd340db7aa6284afc
Reiniel Ravelo <reinielravelo@gmail.com>
2019-05-31 18:34:03 -0400

    Loader added

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -3,7 +3,8 @@
 
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader} from './views/base';
+
 
 /** Gloabal state of the app
  * - Search object
@@ -22,12 +23,15 @@ const controlSearch = async () => {
     // 2) New search object and add to state
     state.search = new Search(query); 
     // 3) Prepare UI for results 
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4) Seach for recipes
    await state.search.getResults();
     // 5) Render results in UI
+    clearLoader();
     searchView.renderResults(state.search.result);
-    console.log(state.search.result);
     }
 }
 
