2f58d039bbd7f290ce5361f96c65f47a251c46b0
Reiniel Ravelo <reinielravelo@gmail.com>
2019-06-06 18:47:41 -0400

    update

--- starter/src/js/config.js
+++ starter/src/js/config.js
@@ -1,3 +1,13 @@
-export const key = "a2f0d6525f0478d675df56f067e06b98";
+//ele export const key = "a2f0d6525f0478d675df56f067e06b98";
 
-//  export const key = "bf329327176803459447269e4bb95237";
+//my key // export const key = "bf329327176803459447269e4bb95237";
+
+//ny key// 
+
+ export const key = "ab467003d802d871de3c2028aeeda0c3";
+
+
+
+
+
+
--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -4,6 +4,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader} from './views/base';
 
 
@@ -84,7 +85,8 @@ const controlRecipe = async () => {
     if (id) {
 
         // Prepare UI for change
-
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
         // Create new recipe object
         state.recipe = new Recipe(id);
 
@@ -96,7 +98,8 @@ const controlRecipe = async () => {
         state.recipe.calcTime(); 
         state.recipe.calcServings();
         // Render recipe 
-        console.log(state.recipe); 
+        clearLoader();
+        recipeView.renderRecipe(state.recipe);
         } catch (err) {
             alert('Error processing recipe!');
         }
