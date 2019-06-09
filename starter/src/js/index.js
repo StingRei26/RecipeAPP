e66ef430bf4147e1408c1cff806fc551e1368fbf
Reiniel Ravelo <reinielravelo@gmail.com>
2019-06-09 15:41:44 -0400

    Shopping list updated and working

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -3,10 +3,11 @@
 
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader} from './views/base';
-
 
 
 /** Gloabal state of the app
@@ -17,6 +18,7 @@ import { elements, renderLoader, clearLoader} from './views/base';
  */
 
 const state = {}; 
+window.state = state;
 
 /**
  * SEARCH CONTROLER 
@@ -44,7 +46,9 @@ const controlSearch = async () => {
     } catch (err) {
         alert('Somthing woring with the search...'); 
         clearLoader(); 
-        }
+    }
+    
+    console.log(state.recipe);
 
     }
 
@@ -84,7 +88,7 @@ const controlRecipe = async () => {
 
     if (id) {
 
-        // Prepare UI for change
+        //Prepare UI for change
         recipeView.clearRecipe();
         renderLoader(elements.recipe);
 
@@ -105,7 +109,8 @@ const controlRecipe = async () => {
         clearLoader();
         recipeView.renderRecipe(state.recipe);
         } catch (err) {
-            alert('Error processing recipe!');
+            alert('Error processing recipe!'); 
+           // console.log(state.recipe); 
         }
     }
 };
@@ -118,6 +123,61 @@ const controlRecipe = async () => {
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe)); 
 
 
+/**
+ * LIST CONTROLER 
+ */
 
 
+const controlList = () => {
+    //Create a new list IF there is none yet
+    if (!state.list) state.list = new List(); 
+
+    // Add each ingredient to the list
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item); 
+    });
+}
+
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+
+        // Delete from UI
+        listView.deleteItem(id); 
+
+        // Handle the count update
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
+// Handling recipe button clicks 
+
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        //Decrease button is clicked 
+        if(state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+
+    } else if (e.target.matches('.btn-increase, .btn-increase *')){
+        state.recipe.updateServings('inc'); 
+        recipeView.updateServingsIngredients(state.recipe);
+    }else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        controlList(); 
+    }
+});
+
+
+window.l = new List(); 
 
--- starter/src/js/views/recipeView.js
+++ starter/src/js/views/recipeView.js
@@ -70,12 +70,12 @@ export const renderRecipe = recipe => {
                     <span class="recipe__info-text"> servings</span>
 
                     <div class="recipe__info-buttons">
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-decrease">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-minus"></use>
                             </svg>
                         </button>
-                        <button class="btn-tiny">
+                        <button class="btn-tiny btn-increase">
                             <svg>
                                 <use href="img/icons.svg#icon-circle-with-plus"></use>
                             </svg>
@@ -91,11 +91,6 @@ export const renderRecipe = recipe => {
             </div>
 
 
-
-
-
-
-
             <div class="recipe__ingredients">
                 <ul class="recipe__ingredient-list">
 
@@ -103,10 +98,7 @@ export const renderRecipe = recipe => {
             
                 </ul>
 
-
-
-
-                <button class="btn-small recipe__btn">
+                <button class="btn-small recipe__btn recipe__btn--add">
                     <svg class="search__icon">
                         <use href="img/icons.svg#icon-shopping-cart"></use>
                     </svg>
@@ -133,3 +125,13 @@ export const renderRecipe = recipe => {
       elements.recipe.insertAdjacentHTML('afterbegin', markup);
 };
 
+export const updateServingsIngredients = recipe => {
+    // Update servings 
+    document.querySelector('.recipe__info-data--people').textContent = recipe.servings; 
+
+    // Update ingredients
+    const countElements = Array.from(document.querySelectorAll('.recipe__count'));
+    countElements.forEach((el, i) => {
+        el.textContent = formatCount(recipe.ingredients[i].count);
+    });
+}
